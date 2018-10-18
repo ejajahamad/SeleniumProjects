e0f0cc3dc071a12542b287acdf2b70f391318bda
EJAJAHAMAD-PC\EJAJAHAMAD <ejuangadi@gmail.com>
2018-10-18 21:06:28 +0530

    Added two integers

--- SeleniumTestProject/src/com/selenium/test/project/Test.java
+++ SeleniumTestProject/src/com/selenium/test/project/Test.java
@@ -3,8 +3,11 @@ package com.selenium.test.project;
 public class Test {
 
 	public static void main(String[] args) {
+		int a=0,b = 0,c;
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		 c=a+b;
+		 System.out.println(c);
 	}
 
 }
